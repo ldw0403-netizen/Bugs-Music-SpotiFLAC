92d7e8f79ee687c45041f991a862f24854cc82f6
ldw0403-netizen <ldw0403@gmail.com>
2026-09-11 06:35:39 +0900

    Add searchTracks and getTrack functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function searchTracks(query, limit) {
+  return [];
+}
+
+function getTrack(trackId) {
+  return null;
+}
+
+registerExtension({
+  searchTracks: searchTracks,
+  getTrack: getTrack
+});
